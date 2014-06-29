ad629da15085ce2eb2b8ea66bf462a3389ffca52
wissam <wissam.r1992@gmail.com>
2014-06-29 05:49:42 -0700

    Delete button has been added

---
+++ layoutT/src/com/example/layoutt/MainActivity.java
@@ -0,0 +1,246 @@
+package com.example.layoutt;
+
+
+import java.util.Scanner;
+
+import note.Note;
+import note.NoteEditer;
+import note.ViewNote;
+
+import dp.NotesDbAdapter;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ContextMenu.ContextMenuInfo;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.AdapterContextMenuInfo;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView.OnItemLongClickListener;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity{
+
+	static listAdap mla = null ;
+	private static NotesDbAdapter mDbHelper;
+	Menu myMenu =  null ;
+	ListView list ;
+	CheckBox c1 ;
+
+	public static NotesDbAdapter getDb(){
+		return mDbHelper;
+	}
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		c1 = (CheckBox) findViewById(R.id.textViewc) ;
+		mDbHelper = new NotesDbAdapter(this);
+		mDbHelper.open();
+//				mDbHelper.dropTable() ;
+//				mDbHelper.createTable() ;
+		//		drawAllNotes();
+		//		mDbHelper.deleteAll();
+		//		mDbHelper.dropTable();
+		list = (ListView) findViewById(R.id.listView);
+		
+		Button b1 = (Button) findViewById(R.id.b1);
+		b1.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View arg0) {
+				
+				Intent intent =  new Intent(MainActivity.this, NoteEditer.class);
+				intent.putExtra("mode", "add") ;
+				startActivity(intent) ;
+//				Intent intent = new Intent(MainActivity.this,Delete_Activity.class);
+//				startActivity(intent) ;
+			}
+		});
+		list.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+					long arg3) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent(MainActivity.this, ViewNote.class);
+				Note note = (Note) mla.getItem(arg2);
+				intent.putExtra("id", note.getId());
+				startActivity(intent);
+
+			}
+
+
+		});
+		list.setOnItemLongClickListener(new OnItemLongClickListener() {
+
+			@Override
+			public boolean onItemLongClick(AdapterView<?> arg0, View arg1,
+					int arg2, long arg3) {
+				registerForContextMenu(arg0);
+				openContextMenu(arg0);	
+				closeContextMenu();
+				return false;
+			}
+		});
+	}
+	@Override
+	public void onCreateContextMenu(ContextMenu menu, View v,
+			ContextMenuInfo menuInfo) {
+		// TODO Auto-generated method stub
+		super.onCreateContextMenu(menu, v, menuInfo);		
+		menu.setHeaderTitle("Menu");
+		getMenuInflater().inflate(R.menu.submenu, menu);
+		
+
+
+	}
+	@Override
+	public boolean onContextItemSelected(MenuItem item) {
+		// TODO Auto-generated method stub
+		return (applyMenuChoice(item));
+
+	}
+	public boolean applyMenuChoice(MenuItem item){
+		AdapterContextMenuInfo info = (AdapterContextMenuInfo) item.getMenuInfo();
+		Note note  = (Note) mla.getItem(info.position);
+		switch (item.getItemId()) {
+		case R.id.menu_delete:
+			buttons.Buttons.delete(note.getId(), getDb()) ;
+			mla = new listAdap(this, 0);
+			list.setAdapter(mla);
+			drawAllNotes();
+			Toast.makeText(getApplicationContext(), "Deleted",
+			Toast.LENGTH_LONG).show();
+			return (true) ;
+		case R.id.menu_edit:
+			Intent intent = new Intent(MainActivity.this, NoteEditer.class);
+			intent.putExtra("mode", "edit") ;
+			intent.putExtra("id", note.getId());
+			startActivity(intent);
+			return true ;
+		default:
+			return super.onContextItemSelected(item);
+		}
+	}
+
+	private void drawAllNotes(){
+		Cursor cursor = mDbHelper.getAllNotes();
+		TextView t1 = (TextView) findViewById(R.id.notes_n) ;
+		t1.setText("Memo ("+cursor.getCount()+")");
+		int i =1 ;
+		while(i <= cursor.getCount()){
+			Scanner con = new Scanner(cursor.getString(2));
+			Note note =  new Note(cursor.getInt(0), cursor.getString(1),con.nextLine() );	
+			MainActivity.mla.add(note,cursor.getString(3));
+			cursor.moveToNext();
+			con.close() ;
+			i++;
+
+
+		}
+		
+
+
+
+	}
+	//	private void drawLast(){
+	//		Cursor cursor = mDbHelper.getAllNotes();
+	//		if (cursor == null)
+	//				return;
+	//		cursor.moveToLast();
+	//		Note note =  new Note(cursor.getInt(0),cursor.getString(1), cursor.getString(2));
+	//		MainActivity.mla.add(note);				
+	//	}
+	//	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		myMenu = menu ;
+		
+		return true;
+	}
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// TODO Auto-generated method stub
+		switch (item.getItemId()) {
+		case R.id.menu_remove:
+			Intent intent = new Intent(MainActivity.this,Delete_Activity.class);
+			startActivity(intent) ;
+			return true ;
+
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+	@Override
+	protected void onStart() {
+		super.onStart();
+		mla = new listAdap(this, 0);
+		list.setAdapter(mla);
+		drawAllNotes();
+	};
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+	}
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+	}
+	@Override
+	protected void onStop() {
+		super.onStop();
+	};
+	@Override
+	protected void onRestart() {
+		super.onRestart();
+
+	};
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		// TODO Auto-generated method stub
+		super.onActivityResult(requestCode, resultCode, data);
+		switch(requestCode){
+		case 1 : if (resultCode==RESULT_OK);
+		}
+	}
+	
+	//	private ActionMode.Callback mActionModeCallback = new ActionMode.Callback() {
+	//		@Override
+	//		public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+	//			// TODO Auto-generated method stub
+	//			return false;
+	//		}
+	//		@Override
+	//		public void onDestroyActionMode(ActionMode mode) {
+	//			// TODO Auto-generated method stub
+	//		}
+	//		@Override
+	//		public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+	//			// TODO Auto-generated method stub
+	//			getMenuInflater().inflate(R.menu.main, menu);
+	//			return true;
+	//		}
+	//		@Override
+	//		public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+	//			// TODO Auto-generated method stub
+	//			return false;
+	//		}
+	//	};
+
+
+}
