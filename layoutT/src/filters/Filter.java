1e1bc2e110a43e16667ec978f6352f3b2a0d441f
wissam <wissam.r1992@gmail.com>
2014-07-08 08:06:15 -0700

    new ver will come up

--- layoutT/src/filters/Filter.java
+++
@@ -1,7 +0,0 @@
-package filters;
-
-import org.opencv.core.Mat;
-
-public interface Filter {
-	void apply(final Mat src, final Mat dst);
-}
--- layoutT/src/filters/TrackFilter.java
+++
@@ -1,9 +0,0 @@
-package filters;
-
-import org.opencv.core.Point;
-
-
-public interface TrackFilter extends Filter{
-	Point[] getTrackedCenters();
-	int getTracked(Point at);
-}
