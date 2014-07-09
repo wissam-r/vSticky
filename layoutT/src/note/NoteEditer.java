24c8ac8aac2c236a23dbfdb460ac450771655c33
jasam-sheja <jasam0jasam0jasam@gmail.com>
2014-07-09 21:55:35 +0300

    sync editing place and mode
    when you add new mode the chose mode button change text to the new
    entered mode
    the same for place

--- layoutT/src/note/NoteEditer.java
+++ layoutT/src/note/NoteEditer.java
@@ -9,7 +9,6 @@ import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.Iterator;
 
 import com.example.layoutt.MyDialog;
 import com.example.layoutt.Notepad;
@@ -20,12 +19,10 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.BitmapFactory.Options;
 import android.location.Location;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
-import android.os.ParcelFileDescriptor.OnCloseListener;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.MenuItem;
@@ -37,7 +34,6 @@ import android.widget.ImageView;
 import android.widget.PopupMenu;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.widget.AdapterView.OnItemClickListener;
 import android.widget.PopupMenu.OnMenuItemClickListener;
 
 public class NoteEditer extends Activity {
@@ -193,21 +189,21 @@ public class NoteEditer extends Activity {
 		super.onActivityResult(requestCode, resultCode, data);
 	}
 
-	OnClickListener add_place = new OnClickListener() {
-
-		@Override
-		public void onClick(View arg0) {
-			new_place() ;
-
-		}
-	};
-	OnClickListener add_mode = new OnClickListener() {
-
-		@Override
-		public void onClick(View arg0) {
-			new_mode();
-		}
-	};
+//	OnClickListener add_place = new OnClickListener() {
+//
+//		@Override
+//		public void onClick(View arg0) {
+//			new_place() ;
+//
+//		}
+//	};
+//	OnClickListener add_mode = new OnClickListener() {
+//
+//		@Override
+//		public void onClick(View arg0) {
+//			new_mode();
+//		}
+//	};
 	OnClickListener choose_mode_l = new OnClickListener() {
 
 		@Override
@@ -295,100 +291,100 @@ public class NoteEditer extends Activity {
 		return place_ID;
 	}
 
-	public void new_mode(){
-		final MyDialog myD = new MyDialog(NoteEditer.this, R.layout.new_mode) ;
-		final EditText mode_name = (EditText) myD.getDialoglayout().findViewById(R.id.new_mode_name) ;
-		Button save_mode = (Button) myD.getDialoglayout().findViewById(R.id.save_mode) ;
-		save_mode.setOnClickListener(new OnClickListener() {
+//	public void new_mode(){
+//		final MyDialog myD = new MyDialog(NoteEditer.this, R.layout.new_mode) ;
+//		final EditText mode_name = (EditText) myD.getDialoglayout().findViewById(R.id.new_mode_name) ;
+//		Button save_mode = (Button) myD.getDialoglayout().findViewById(R.id.save_mode) ;
+//		save_mode.setOnClickListener(new OnClickListener() {
+//
+//			@Override
+//			public void onClick(View arg0) {
+//
+//				if (!mode_name.getText().toString().equals("")){
+//					Notepad.getDb().insertMode(mode_name.getText().toString()) ;
+//					Cursor cursor = Notepad.getDb().getAllModes() ;
+//					Toast.makeText(getBaseContext(), String.valueOf(cursor.getCount()), Toast.LENGTH_LONG).show();
+//					myD.getAlertDialog().cancel() ;
+//				}
+//			}
+//		}) ;
+//		myD.getAlertDialog().show() ;
+//	}
 
-			@Override
-			public void onClick(View arg0) {
-
-				if (!mode_name.getText().toString().equals("")){
-					Notepad.getDb().insertMode(mode_name.getText().toString()) ;
-					Cursor cursor = Notepad.getDb().getAllModes() ;
-					Toast.makeText(getBaseContext(), String.valueOf(cursor.getCount()), Toast.LENGTH_LONG).show();
-					myD.getAlertDialog().cancel() ;
-				}
-			}
-		}) ;
-		myD.getAlertDialog().show() ;
-	}
-
-	public void new_place(){
-		final MyDialog myD = new MyDialog(NoteEditer.this, R.layout.new_place) ;
-		TextView currentGps = (TextView) myD.getDialoglayout().findViewById(R.id.current_pos);
-		TextView currentGpsNet = (TextView) myD.getDialoglayout().findViewById(R.id.current_posnet);
-		Button saveB =  (Button) myD.getDialoglayout().findViewById(R.id.save_place) ;
-		final EditText rangeGPS = (EditText) myD.getDialoglayout().findViewById(R.id.range_gps) ;
-		final EditText placeName = (EditText) myD.getDialoglayout().findViewById(R.id.name_place) ;
-
-
-		final GPSProvider gps = new GPSProvider(NoteEditer.this, currentGps) ;
-		final NetworkProvider netGPS = new NetworkProvider(NoteEditer.this, currentGpsNet) ;
-
-		saveB.setOnClickListener(new OnClickListener() {
-//TODO look here and use it in the other place
-			@Override
-			public void onClick(View arg0) {
-				Place newplace ;
-				final int minRaduis = 10;
-				if ((GPSProvider.isGPS_ConToSatil()) && (gps.getLatitude()!=null)&&(gps.getLongitude()!=null))
-				{
-					try{
-						newplace = new Place(-1, placeName.getText().toString(), gps.getLongitude(), gps.getLatitude(), Integer.parseInt(rangeGPS.getText().toString()));
-					}catch(NumberFormatException e){
-						Toast.makeText(getBaseContext(), "Plz enter unsigned number net", Toast.LENGTH_LONG).show() ;
-						return;
-					}
-				}
-				else if ((NetworkProvider.isInternet_con()) && (netGPS.getLatitude()!=null)&&(netGPS.getLongitude()!=null))
-				{
-					try{
-						newplace = new Place(-1, placeName.getText().toString(), netGPS.getLongitude(), netGPS.getLatitude(), Integer.parseInt(rangeGPS.getText().toString()));
-					}catch(NumberFormatException e){
-						Toast.makeText(getBaseContext(), "Plz enter unsigned number net", Toast.LENGTH_LONG).show() ;
-						return;
-					}
-				}else{
-					Toast.makeText(getBaseContext(), "fail", Toast.LENGTH_LONG).show() ;
-					return;
-				}
-
-				if ((newplace.getRaduis() >= minRaduis)&&(!newplace.getName().equals("")) ){
-					Cursor cursor = Notepad.getDb().getAllPlaces() ;
-					Region  reg;
-
-					int i = 0 ;
-					while (i < cursor.getCount())
-					{
-						reg = new Region(cursor.getDouble(2), cursor.getDouble(3), cursor.getInt(4));
-						if (reg.isInside(newplace.getRegion()))
-						{
-							if(reg.isInside(newplace.getRegion().getLoc())){
-								Toast.makeText(getBaseContext(), "fail: you are inside other place: "+cursor.getString(1), Toast.LENGTH_LONG).show();
-							}else{
-								Toast.makeText(getBaseContext(), "fail: region collision with "+cursor.getString(1), Toast.LENGTH_LONG).show();
-							}
-							return ;
-						}
-						if(cursor.getString(1).equals(newplace.getName())){
-							Toast.makeText(getBaseContext(), "fail: chose other name for this palce", Toast.LENGTH_LONG).show();
-							return;
-						}
-						i++ ;
-						cursor.moveToNext() ;
-					}
-					Notepad.getDb().insertPlace(newplace.getName(), newplace.getX(), newplace.getY(), newplace.getRaduis()) ;
-					Toast.makeText(getBaseContext(), "success", Toast.LENGTH_LONG).show() ;
-					myD.getAlertDialog().cancel() ;
-				}
-
-			}
-		});
-
-		myD.getAlertDialog().show() ;
-	}
+//	public void new_place(){
+//		final MyDialog myD = new MyDialog(NoteEditer.this, R.layout.new_place) ;
+//		TextView currentGps = (TextView) myD.getDialoglayout().findViewById(R.id.current_pos);
+//		TextView currentGpsNet = (TextView) myD.getDialoglayout().findViewById(R.id.current_posnet);
+//		Button saveB =  (Button) myD.getDialoglayout().findViewById(R.id.save_place) ;
+//		final EditText rangeGPS = (EditText) myD.getDialoglayout().findViewById(R.id.range_gps) ;
+//		final EditText placeName = (EditText) myD.getDialoglayout().findViewById(R.id.name_place) ;
+//
+//
+//		final GPSProvider gps = new GPSProvider(NoteEditer.this, currentGps) ;
+//		final NetworkProvider netGPS = new NetworkProvider(NoteEditer.this, currentGpsNet) ;
+//
+//		saveB.setOnClickListener(new OnClickListener() {
+////TODO look here and use it in the other place
+//			@Override
+//			public void onClick(View arg0) {
+//				Place newplace ;
+//				final int minRaduis = 10;
+//				if ((GPSProvider.isGPS_ConToSatil()) && (gps.getLatitude()!=null)&&(gps.getLongitude()!=null))
+//				{
+//					try{
+//						newplace = new Place(-1, placeName.getText().toString(), gps.getLongitude(), gps.getLatitude(), Integer.parseInt(rangeGPS.getText().toString()));
+//					}catch(NumberFormatException e){
+//						Toast.makeText(getBaseContext(), "Plz enter unsigned number net", Toast.LENGTH_LONG).show() ;
+//						return;
+//					}
+//				}
+//				else if ((NetworkProvider.isInternet_con()) && (netGPS.getLatitude()!=null)&&(netGPS.getLongitude()!=null))
+//				{
+//					try{
+//						newplace = new Place(-1, placeName.getText().toString(), netGPS.getLongitude(), netGPS.getLatitude(), Integer.parseInt(rangeGPS.getText().toString()));
+//					}catch(NumberFormatException e){
+//						Toast.makeText(getBaseContext(), "Plz enter unsigned number net", Toast.LENGTH_LONG).show() ;
+//						return;
+//					}
+//				}else{
+//					Toast.makeText(getBaseContext(), "fail", Toast.LENGTH_LONG).show() ;
+//					return;
+//				}
+//
+//				if ((newplace.getRaduis() >= minRaduis)&&(!newplace.getName().equals("")) ){
+//					Cursor cursor = Notepad.getDb().getAllPlaces() ;
+//					Region  reg;
+//
+//					int i = 0 ;
+//					while (i < cursor.getCount())
+//					{
+//						reg = new Region(cursor.getDouble(2), cursor.getDouble(3), cursor.getInt(4));
+//						if (reg.isInside(newplace.getRegion()))
+//						{
+//							if(reg.isInside(newplace.getRegion().getLoc())){
+//								Toast.makeText(getBaseContext(), "fail: you are inside other place: "+cursor.getString(1), Toast.LENGTH_LONG).show();
+//							}else{
+//								Toast.makeText(getBaseContext(), "fail: region collision with "+cursor.getString(1), Toast.LENGTH_LONG).show();
+//							}
+//							return ;
+//						}
+//						if(cursor.getString(1).equals(newplace.getName())){
+//							Toast.makeText(getBaseContext(), "fail: chose other name for this palce", Toast.LENGTH_LONG).show();
+//							return;
+//						}
+//						i++ ;
+//						cursor.moveToNext() ;
+//					}
+//					Notepad.getDb().insertPlace(newplace.getName(), newplace.getX(), newplace.getY(), newplace.getRaduis()) ;
+//					Toast.makeText(getBaseContext(), "success", Toast.LENGTH_LONG).show() ;
+//					myD.getAlertDialog().cancel() ;
+//				}
+//
+//			}
+//		});
+//
+//		myD.getAlertDialog().show() ;
+//	}
 
 
 
@@ -461,17 +457,19 @@ public class NoteEditer extends Activity {
 				}
 			});
 			
-			Button new_place = (Button) myD.getDialoglayout().findViewById(R.id.new_loc);
-			new_place.setOnClickListener(add_place) ;
-			
-			Button new_mode = (Button) myD.getDialoglayout().findViewById(R.id.add_mode) ;
-			new_mode.setOnClickListener(add_mode) ;
+			TextView usedLocation = (TextView) myD.getDialoglayout().findViewById(R.id.location_mode);
 			
 			Button choose_mode = (Button) myD.getDialoglayout().findViewById(R.id.mode_chose) ;
 			choose_mode.setOnClickListener(choose_mode_l) ;
 			
 			Button choose_place = (Button) myD.getDialoglayout().findViewById(R.id.loc_chose) ;
 			choose_place.setOnClickListener(choose_place_l);
+			
+			Button new_place = (Button) myD.getDialoglayout().findViewById(R.id.new_loc);
+			new_place.setOnClickListener(new NewPlaceAdder(NoteEditer.this, usedLocation)) ;
+			
+			Button new_mode = (Button) myD.getDialoglayout().findViewById(R.id.add_mode) ;
+			new_mode.setOnClickListener(new NewModeAdder(NoteEditer.this, choose_mode)) ;
 			
 			Button done_tag = (Button) myD.getDialoglayout().findViewById(R.id.done_tag);
 			done_tag.setOnClickListener(new OnClickListener() {
@@ -483,14 +481,16 @@ public class NoteEditer extends Activity {
 				}
 			}) ;
 			
-			TextView usedLocation = (TextView) myD.getDialoglayout().findViewById(R.id.location_mode);
+			
 			
 			place_ID = getGPSID();
 			Log.d("place","getGPSID :: "+place_ID);
-			if(place_ID!=-1){
+			if(place_ID>-1){
 				new_place.setEnabled(false);
 				choose_place.setEnabled(false);
 				usedLocation.setText(Notepad.getDb().getPlacesById(place_ID).getString(1));
+			}else if(place_ID==-2){//we know our gps locatoin but not where we are
+				choose_place.setEnabled(false);
 			}
 			myD.getAlertDialog().show() ;
 		}
@@ -527,7 +527,7 @@ public class NoteEditer extends Activity {
 				}
 			}while(cursor.moveToNext());
 		}
-		return -1;
+		return -2;
 	}
 
 	@Override
@@ -537,4 +537,143 @@ public class NoteEditer extends Activity {
 
 }
 
+class NewModeAdder implements OnClickListener{
+	public NewModeAdder(Activity activity){
+		this.activity = activity;
+	}
+	public NewModeAdder(Activity activity, Button b){
+		button = b;
+		this.activity = activity;
+	}
+	
+	Button button;
+	Activity activity;
+	@Override
+	public void onClick(View v) {
+		final MyDialog myD = new MyDialog(activity, R.layout.new_mode) ;
+		final EditText mode_name = (EditText) myD.getDialoglayout().findViewById(R.id.new_mode_name) ;
+		Button save_mode = (Button) myD.getDialoglayout().findViewById(R.id.save_mode) ;
+		save_mode.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+
+				if (!mode_name.getText().toString().equals("")){
+					Notepad.getDb().insertMode(mode_name.getText().toString()) ;
+					Cursor cursor = Notepad.getDb().getAllModes() ;
+					Toast.makeText(activity, String.valueOf(cursor.getCount()), Toast.LENGTH_LONG).show();
+					myD.getAlertDialog().cancel() ;
+					if(button!= null){
+						button.setText(mode_name.getText().toString());
+					}
+				}
+			}
+		}) ;
+		myD.getAlertDialog().show() ;
+		
+	}
+	
+}
+
+class NewPlaceAdder implements OnClickListener{
+	public NewPlaceAdder(Activity activity){
+		this.activity = activity;
+	}
+	public NewPlaceAdder(Activity activity, Button b){
+		button = b;
+		this.activity = activity;
+	}
+	public NewPlaceAdder(Activity activity, TextView t){
+		textview = t;
+		this.activity = activity;
+	}
+	
+	Button button;
+	TextView textview;
+	Activity activity;
+	@Override
+	public void onClick(View v) {
+		final MyDialog myD = new MyDialog(activity, R.layout.new_place) ;
+		TextView currentGps = (TextView) myD.getDialoglayout().findViewById(R.id.current_pos);
+		TextView currentGpsNet = (TextView) myD.getDialoglayout().findViewById(R.id.current_posnet);
+		Button saveB =  (Button) myD.getDialoglayout().findViewById(R.id.save_place) ;
+		final EditText rangeGPS = (EditText) myD.getDialoglayout().findViewById(R.id.range_gps) ;
+		final EditText placeName = (EditText) myD.getDialoglayout().findViewById(R.id.name_place) ;
+
+
+		final GPSProvider gps = new GPSProvider(activity, currentGps) ;
+		final NetworkProvider netGPS = new NetworkProvider(activity, currentGpsNet) ;
+		final View fv = v;
+		saveB.setOnClickListener(new OnClickListener() {
+			//TODO look here and use it in the other place
+			@Override
+			public void onClick(View arg0) {
+				Place newplace ;
+				final int minRaduis = 10;
+				if ((GPSProvider.isGPS_ConToSatil()) && (gps.getLatitude()!=null)&&(gps.getLongitude()!=null))
+				{
+					try{
+						newplace = new Place(-1, placeName.getText().toString(), gps.getLongitude(), gps.getLatitude(), Integer.parseInt(rangeGPS.getText().toString()));
+					}catch(NumberFormatException e){
+						Toast.makeText(activity, "Plz enter unsigned number net", Toast.LENGTH_LONG).show() ;
+						return;
+					}
+				}
+				else if ((NetworkProvider.isInternet_con()) && (netGPS.getLatitude()!=null)&&(netGPS.getLongitude()!=null))
+				{
+					try{
+						newplace = new Place(-1, placeName.getText().toString(), netGPS.getLongitude(), netGPS.getLatitude(), Integer.parseInt(rangeGPS.getText().toString()));
+					}catch(NumberFormatException e){
+						Toast.makeText(activity, "Plz enter unsigned number net", Toast.LENGTH_LONG).show() ;
+						return;
+					}
+				}else{
+					Toast.makeText(activity, "fail", Toast.LENGTH_LONG).show() ;
+					return;
+				}
+
+				if ((newplace.getRaduis() >= minRaduis)&&(!newplace.getName().equals("")) ){
+					Cursor cursor = Notepad.getDb().getAllPlaces() ;
+					Region  reg;
+
+					int i = 0 ;
+					while (i < cursor.getCount())
+					{
+						reg = new Region(cursor.getDouble(2), cursor.getDouble(3), cursor.getInt(4));
+						if (reg.isInside(newplace.getRegion()))
+						{
+							if(reg.isInside(newplace.getRegion().getLoc())){
+								Toast.makeText(activity, "fail: you are inside other place: "+cursor.getString(1), Toast.LENGTH_LONG).show();
+							}else{
+								Toast.makeText(activity, "fail: region collision with "+cursor.getString(1), Toast.LENGTH_LONG).show();
+							}
+							return ;
+						}
+						if(cursor.getString(1).equals(newplace.getName())){
+							Toast.makeText(activity, "fail: chose other name for this palce", Toast.LENGTH_LONG).show();
+							return;
+						}
+						i++ ;
+						cursor.moveToNext() ;
+					}
+					Notepad.getDb().insertPlace(newplace.getName(), newplace.getX(), newplace.getY(), newplace.getRaduis()) ;
+					Toast.makeText(activity, "success", Toast.LENGTH_LONG).show() ;
+					myD.getAlertDialog().cancel() ;
+					if(button!=null){
+						button.setText(newplace.getName());
+					}
+					if(textview!=null){
+						textview.setText(newplace.getName());
+					}
+					fv.setEnabled(false);
+				}
+				
+			}
+		});
+
+		myD.getAlertDialog().show() ;
+		
+	}
+	
+}
 
